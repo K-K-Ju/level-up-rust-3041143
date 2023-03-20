986412d5e1abf152ac20a03467141fa2cf44e29c
K-K-Ju <andrunivermail@gmail.com>
2023-03-20 11:00:25 +0200

    info() impled for any Debugable struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-fn info(a: &T) {
-    todo!();
+use core::fmt::Debug;
+
+fn info<T>(a: &T) 
+where T: Debug + ?Sized{
+    println!("{:?}", a);
 }
 
 fn main() {
@@ -9,15 +12,15 @@ fn main() {
     info(&b);
 
     // Advanced 1
-    // use std::ffi::CString;
+    use std::ffi::CString;
     
-    // let c = CString::new("?").unwrap();
-    // info(&input);
+    let c = CString::new("cString").unwrap();
+    info(&c);
 
     // Advanced 2
-    // use std::path::Path;
-    // let d = Path::new("/tmp/linkedin-learning");
-    // info(d);
+    use std::path::Path;
+    let d = Path::new("/tmp/linkedin-learning");
+    info(d);
 }
 
 
@@ -33,22 +36,22 @@ fn string() {
     info(&input);
 }
 
-// #[test]
-// fn chars() {
-//     let input = 'r';
-//     info(&input);
-// }
+#[test]
+fn chars() {
+    let input = 'r';
+    info(&input);
+}
 
-// #[test]
-// fn cstring() {
-//     use std::ffi::{CString};
-//     let input = CString::new("Rust").unwrap();
-//     info(&input);
-// }
+#[test]
+fn cstring() {
+    use std::ffi::{CString};
+    let input = CString::new("Rust").unwrap();
+    info(&input);
+}
 
-// #[test]
-// fn path() {
-//     use std::path::Path;
-//     let input = Path::new("/tmp/rust");
-//     info(input);
-// }
+#[test]
+fn path() {
+    use std::path::Path;
+    let input = Path::new("/tmp/rust");
+    info(input);
+}
